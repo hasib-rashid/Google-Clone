9b4031fd1a081cc7c240173a8b74b88a70cb9c05
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-09 23:38:31 +0600

    👑Google Logo done

--- src/pages/components/MainPage.tsx
+++ src/pages/components/MainPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainPage = () => {
     return (
-        <main>
+        <main className="MainPage">
             <header className="main_header">
                 <div className="main_header_comp">
                     <p>Gmail</p>
@@ -20,6 +20,10 @@ const MainPage = () => {
                     <img className="user-icon" src="https://img.icons8.com/ios-filled/85/000000/user-male-circle.png" alt="User" />
                 </div>
             </header>
+
+            <main className="main-google-comp">
+                <img className="google-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png" alt="" />
+            </main>
         </main>
     )
 }
